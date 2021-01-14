028b9cb12534fe4d0607f7b8d0c0c563d8d3f563
mjoghenemaega <mjoghenemaega@gmail.com>
2021-01-14 21:17:03 +0100

    first commit wit all carts and view working well

---
+++ Megastore/urls.py
@@ -0,0 +1,44 @@
+"""saulgadgets URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from apps.cart.views import cart_detail
+from apps.core.views import frontpage, contact, about
+from apps.store.views import product_detail, category_detail
+
+from apps.store.api import api_add_to_cart, api_remove_from_cart, api_checkout
+
+urlpatterns = [
+    path('', frontpage, name='frontpage'),
+    path('cart/', cart_detail, name='cart'),
+    path('contact/', contact, name='contact'),
+    path('about/', about, name='about'),
+    path('admin/', admin.site.urls),
+
+    # API
+
+    path('api/add_to_cart/', api_add_to_cart, name='api_add_to_cart'),
+    path('api/remove_from_cart/', api_remove_from_cart, name='api_remove_from_cart'),
+    path('api/checkout/', api_checkout, name='api_checkout'),
+
+    # Store
+
+    path('<slug:category_slug>/<slug:slug>/', product_detail, name='product_detail'),
+    path('<slug:slug>/', category_detail, name='category_detail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ apps/core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+from apps.store.models import Product
+
+def frontpage(request):
+    products = Product.objects.all()
+
+    context = {
+        'products': products
+    }
+
+    return render(request, 'frontpage.html', context)
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
